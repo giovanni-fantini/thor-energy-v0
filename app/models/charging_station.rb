4c4939dd14cdeab2e99a0c3856f86a03febca090
Giovanni Fantini <fantini.giovanni@gmail.com>
2018-05-28 16:45:36 +0100

    Charging station model ready

--- app/models/charging_station.rb
+++ app/models/charging_station.rb
@@ -1,2 +1,16 @@
 class ChargingStation < ApplicationRecord
+  validates :capacity, :address, :longitude, :latitude, :status, presence: true
+  validates :capacity, inclusion: {in: (1..80).to_a, message: "%{value} is not a valid capacity (min 1, max 80)"}
+  validates :capacity, numericality: { only_integer: true }
+  validates :longitude, inclusion: {in: (-180..180).to_a }
+  validates :latitude, inclusion: {in: (-90..90).to_a }
+  validates :status, inclusion: {in: [true, false] }
+  validate :battery_count_cannot_be_higher_than_capacity
+
+  private
+
+  def battery_count_cannot_be_higher_than_capacity
+    return true if (battery_count == "" || battery_count.nil?)
+    errors.add(:battery_count, "Can't be higher than capacity") if battery_count > capacity
+  end
 end
