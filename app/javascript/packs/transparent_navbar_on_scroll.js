784425fa161deeb1e0e8fed619b62df9bf12d784
Sam Jenkins <sam.jenko@hotmail.com>
2018-06-06 19:49:26 +0100

    navbar transparency bug fixed

--- app/javascript/packs/transparent_navbar_on_scroll.js
+++ app/javascript/packs/transparent_navbar_on_scroll.js
@@ -1,10 +1,10 @@
 const navbar = document.querySelector('.navbar-wagon');
 if (navbar) {
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight) {
-      navbar.classList.remove('navbar-wagon-transparent');
-    } else {
+    if (window.scrollY < window.innerHeight) {
       navbar.classList.add('navbar-wagon-transparent');
+    } else {
+      navbar.classList.remove('navbar-wagon-transparent');
     }
   });
 }
