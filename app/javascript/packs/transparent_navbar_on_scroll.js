ddc650ccb99dd3b6fdd30af09094703ddcc53f03
Sam Jenkins <sam.jenko@hotmail.com>
2018-06-06 19:39:58 +0100

    reversed navbar transparency behaviour

--- app/javascript/packs/transparent_navbar_on_scroll.js
+++ app/javascript/packs/transparent_navbar_on_scroll.js
@@ -2,9 +2,9 @@ const navbar = document.querySelector('.navbar-wagon');
 if (navbar) {
   window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight) {
-      navbar.classList.add('navbar-wagon-transparent');
-    } else {
       navbar.classList.remove('navbar-wagon-transparent');
+    } else {
+      navbar.classList.add('navbar-wagon-transparent');
     }
   });
 }
