ef2dbdf589be5e9447bf5e4fb07f6e9b8d23311f
Sam Jenkins <sam.jenko@hotmail.com>
2018-06-06 13:55:42 +0100

    transparent_navbar_on_scroll.js can now be included on pages views which require transparent navbar feature

---
+++ app/javascript/packs/transparent_navbar_on_scroll.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('.navbar-wagon');
+if (navbar) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= window.innerHeight) {
+      navbar.classList.add('navbar-wagon-transparent');
+    } else {
+      navbar.classList.remove('navbar-wagon-transparent');
+    }
+  });
+}
