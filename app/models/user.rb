102ad54714d5c8d125f708eb78a6fe74e1fd352d
Sam Jenkins <sam.jenko@hotmail.com>
2018-06-07 23:57:05 +0100

    user show implemented and styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :rentals
+
   # For activeadmin use:
   def name
     "#{id} - #{email}"
+  end
+
+  def open_rentals
+    self.rentals.select { |rental| rental.status == "open" }
+  end
+
+  def closed_rentals
+    self.rentals.select { |rental| rental.status == "closed" }
   end
 end
