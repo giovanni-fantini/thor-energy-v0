475379c3190c681e2ee123c45794ad387c60c7d5
Sam Jenkins <sam.jenko@hotmail.com>
2018-06-07 18:52:14 +0100

    added clear title labels for pickup and dropoff waypoints within directions list

--- app/javascript/components/journey_planner.js
+++ app/javascript/components/journey_planner.js
@@ -11,6 +11,12 @@ const journeyPlanner = (modeTransport, marker, markers, map, userLocation) => {
       const destination = document.getElementById('destination').value
       document.querySelector('.content').innerHTML = ""
       routingIntelligence(modeTransport, marker, destination, markers, map, userLocation)
+      setTimeout(() => {
+      const waypoints = document.querySelectorAll(".adp-placemark")
+      waypoints[1].insertAdjacentHTML("beforebegin", `<h1 class="text-center direction-insert">Battery Pickup:</h1>`)
+      waypoints[2].insertAdjacentHTML("beforebegin", `<h1 class="text-center direction-insert">Battery Dropoff:</h1>`)
+      document.querySelectorAll(".adp-placemark img").forEach((marker) => {marker.remove()})
+      }, 1000)
     })
   })
 }
